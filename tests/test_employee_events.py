392047f6a54618b8b59106082190fdc547daa8b1
Stefan Schonauer <stefan.schonauer@googlemail.com>
2025-04-17 22:10:19 +0200

    #4
    #4 update

--- tests/test_employee_events.py
+++ tests/test_employee_events.py
@@ -5,15 +5,18 @@ from pathlib import Path
 # variable set to the absolute path
 # for the root of this project
 #### YOUR CODE HERE
-project_root = Path(__file__).parent
+project_root = Path(__file__).parents[1]
 
 # apply the pytest fixture decorator
 # to a `db_path` function
 #### YOUR CODE HERE
+@pytest.fixture
+def db_path():
     
     # Using the `project_root` variable
     # return a pathlib object for the `employee_events.db` file
     #### YOUR CODE HERE
+    return project_root / 'python-package' / 'employee_events' / 'employee_events.db'
 
 # Define a function called
 # `test_db_exists`
@@ -22,11 +25,13 @@ project_root = Path(__file__).parent
 # the creates the "fixture" for
 # the database's filepath
 #### YOUR CODE HERE
+def test_db_exists(db_path):
     
     # using the pathlib `.is_file` method
     # assert that the sqlite database file exists
     # at the location passed to the test_db_exists function
     #### YOUR CODE HERE
+    assert db_path.is_file()
 
 @pytest.fixture
 def db_conn(db_path):
@@ -43,28 +48,33 @@ def table_names(db_conn):
 # This function should receive the `table_names`
 # fixture as an argument
 #### YOUR CODE HERE
+def test_employee_table_exists(table_names):
 
     # Assert that the string 'employee'
     # is in the table_names list
     #### YOUR CODE HERE
+    assert 'employee' in table_names
 
 # Define a test function called
 # `test_team_table_exists`
 # This function should receive the `table_names`
 # fixture as an argument
 #### YOUR CODE HERE
+def test_team_table_exists(table_names):
 
     # Assert that the string 'team'
     # is in the table_names list
     #### YOUR CODE HERE
+    assert 'team' in table_names
 
 # Define a test function called
 # `test_employee_events_table_exists`
 # This function should receive the `table_names`
 # fixture as an argument
 #### YOUR CODE HERE
+def test_employee_events_table_exists(table_names):
 
     # Assert that the string 'employee_events'
     # is in the table_names list
     #### YOUR CODE HERE
-
+    assert 'employee_events' in table_names
